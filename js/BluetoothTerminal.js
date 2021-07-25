d15867ad3c03f58f0386099e3079789059209766
Vo Thuong Nguyen <49666553+nguyenthuongvo@users.noreply.github.com>
2021-07-25 13:05:04 +0700

    Minor UI update

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -112,15 +112,16 @@
    * Disconnect from the connected device.
    */
   disconnect() {
-    this._disconnectFromDevice(this._device);
+    this._stopNotifications(this._characteristic).then(value => {
+      this._disconnectFromDevice(this._device);
 
-    if (this._characteristic) {
-      this._characteristic.removeEventListener('characteristicvaluechanged',
-          this._boundHandleCharacteristicValueChanged);
-      this._characteristic = null;
-    }
-
-    this._device = null;
+      if (this._characteristic) {
+        this._characteristic.removeEventListener('characteristicvaluechanged',
+            this._boundHandleCharacteristicValueChanged);
+        this._characteristic = null;
+      }
+      this._device = null;
+    })
   }
 
   /**
@@ -149,6 +150,16 @@
     (green.length ==  1 ? "0" + green: green)  + ""  + 
     (blue.length ==  1 ? "0" + blue : blue) + "1E"; 
     return this._writeToCharacteristic(this._characteristic, solidColor);
+  }
+
+    /**
+   * Set LED Strip Brightness
+   * @param {Object} value range 0 -255
+   */
+  setBrightness(value) {
+    const hexValue = value.toString(16);
+    const command = (hexValue.length ==  1 ? "0" + hexValue : hexValue) + "00002A";
+    return this._writeToCharacteristic(this._characteristic, command);
   }
 
   getDeviceInfo() {
@@ -218,10 +229,72 @@
     this._log('Requesting bluetooth device... with service1 ' + this._serviceUuid);
 	
     return navigator.bluetooth.requestDevice({
-		
-      filters: [{
-        name: 'TranDecor'
-      }]
+      filters: [
+        { name: '' },
+        { namePrefix: '0' },
+        { namePrefix: '1' },
+        { namePrefix: '2' },
+        { namePrefix: '3' },
+        { namePrefix: '4' },
+        { namePrefix: '5' },
+        { namePrefix: '6' },
+        { namePrefix: '7' },
+        { namePrefix: '8' },
+        { namePrefix: '9' },
+        { namePrefix: 'a' },
+        { namePrefix: 'b' },
+        { namePrefix: 'c' },
+        { namePrefix: 'd' },
+        { namePrefix: 'e' },
+        { namePrefix: 'f' },
+        { namePrefix: 'g' },
+        { namePrefix: 'h' },
+        { namePrefix: 'i' },
+        { namePrefix: 'j' },
+        { namePrefix: 'k' },
+        { namePrefix: 'l' },
+        { namePrefix: 'm' },
+        { namePrefix: 'n' },
+        { namePrefix: 'o' },
+        { namePrefix: 'p' },
+        { namePrefix: 'q' },
+        { namePrefix: 'r' },
+        { namePrefix: 's' },
+        { namePrefix: 't' },
+        { namePrefix: 'u' },
+        { namePrefix: 'v' },
+        { namePrefix: 'w' },
+        { namePrefix: 'x' },
+        { namePrefix: 'y' },
+        { namePrefix: 'z' },
+        { namePrefix: 'A' },
+        { namePrefix: 'B' },
+        { namePrefix: 'C' },
+        { namePrefix: 'D' },
+        { namePrefix: 'E' },
+        { namePrefix: 'F' },
+        { namePrefix: 'G' },
+        { namePrefix: 'H' },
+        { namePrefix: 'I' },
+        { namePrefix: 'J' },
+        { namePrefix: 'K' },
+        { namePrefix: 'L' },
+        { namePrefix: 'M' },
+        { namePrefix: 'N' },
+        { namePrefix: 'O' },
+        { namePrefix: 'P' },
+        { namePrefix: 'Q' },
+        { namePrefix: 'R' },
+        { namePrefix: 'S' },
+        { namePrefix: 'T' },
+        { namePrefix: 'U' },
+        { namePrefix: 'V' },
+        { namePrefix: 'W' },
+        { namePrefix: 'X' },
+        { namePrefix: 'Y' },
+        { namePrefix: 'Z' }
+      ],
+      optionalServices: [65504]
     }).then((device) => {
           this._log('"' + device.name + '" bluetooth device selected');
 
@@ -326,11 +399,7 @@
    */
   _handleCharacteristicValueChanged(event) {
     const data = event.target.value;
-    // const str = "";
-    // for ( var i = 0; i < 13; i++) {
-    //   str+= data.getUint8(i).toString() + " ";
-    // }
-    this.receive(data.getUint8(7));
+    this.receive(data); // Led count
   }
 
   /**
--- js/Main.js
+++ js/Main.js
@@ -1,10 +1,53 @@
 var r = g = b = 255;
-var serviveUuid = 0xFFE0
-var characteristicUuid = 0xFFE1
+var br = 255;
+var isSendData = false;
+var slider
+var serviveUuid = 0xFFE0;
+var characteristicUuid = 0xFFE1;
 const terminal = new BluetoothTerminal(serviveUuid,characteristicUuid,'\n','\n');
 
 terminal.receive = function(data) {
-  $('.battery_level').text(data);
+  isSendData = false;
+
+  this._log("Enabled: " + data.getUint8(0));
+  this._log("Preset: " + data.getUint8(1));
+  this._log("Bright: " + data.getUint8(3));
+  this._log("R: " + data.getUint8(8));
+  this._log("G: " + data.getUint8(9));
+  this._log("B: " + data.getUint8(10));
+  this._log("W: " + data.getUint8(11));
+
+  br = data.getUint8(3);
+  r = data.getUint8(8);
+  g = data.getUint8(9);
+  b = data.getUint8(10);
+  
+
+  $('.battery_level').text(data.getUint8(7)); // LED COUNT
+  setPowerUI(data.getUint8(0)); // Enabled
+
+
+  //Check HOT MODE
+  if (data.getUint8(8) == 255 && data.getUint8(9) == 0 && data.getUint8(10) == 0) {
+    $('#hotmode').trigger('click');
+  }
+
+  //Check COLD MODE
+  if (data.getUint8(8) == 0 && data.getUint8(9) == 255 && data.getUint8(10) == 255) {
+    $('#coldmode').trigger('click');
+  }
+
+  //Check HOME MODE
+  if (data.getUint8(8) == 242 && data.getUint8(9) == 116 && data.getUint8(10) == 5) {
+    $('#homemode').trigger('click');
+  }
+
+  // Check Brightness
+  slider.slider( "value", br );
+
+  //Set isSendData for webbluetooth send value
+  isSendData = true;
+
 };
 
 
@@ -13,20 +56,25 @@ $(document).ready(function() {
   // Helpers.
   const defaultDeviceName = 'No device is connected';
 
-  const slider = $( "#slider" ).slider({
+  slider = $( "#slider" ).slider({
     animate: "fast",
     orientation: "vertical",
     range: "min",
-    value: 1,
+    value: 0,
     min: 1,
-    max: 100,
+    max: 255,
     disabled: true,
-    change: function( event, ui ) {
-        console.log(slider.slider('value'));
+    change: async function( event, ui ) {
+        const sliderValue = slider.slider('value');
+        if (isSendData) {
+          await terminal.setBrightness(sliderValue);
+        }
     }
   });
 
   $('#connect').on('click', function() {
+    spinner('block');
+    isSendData = false;
     terminal.connect().
     then(() => {
       $('#device-name').text(terminal.getDeviceName() + " is connected");
@@ -45,6 +93,7 @@ $(document).ready(function() {
   $('#disconnect').on('click', function() {
     spinner('block');
     terminal.disconnect();
+    isSendData = false;
     $('#device-name').text(defaultDeviceName);
     $('#connect').css('display','block');
     $('#disconnect').css('display','none');
@@ -52,48 +101,65 @@ $(document).ready(function() {
     spinner('none');
   });
 
-  $('#power').on('click', function() {
-    const sliderValue =  slider.slider( "value" );
-    console.log(sliderValue);
-    if (sliderValue > 1) {
-      slider.slider( "value", 0 );
-      slider.slider( "disable" );
-      $('#hotmode, #coldmode, #homemode').attr('disabled',true);
-
-
-      r = 0, g = 0, b = 0;
-      terminal.setPowerOff();
+  $('#power').on('click', async function() {
+    const isDisabled =  slider.hasClass('ui-state-disabled');
+    if (isDisabled) {
+      await setPowerUI(true);
     } else {
-      slider.slider( "value", 79 );
-      slider.slider( "enable" );
-      $('#hotmode, #coldmode, #homemode').attr('disabled',false);
-      r = 255, g = 255, b = 255;
-      terminal.setPowerOn();
+      await setPowerUI(false);
     }
   });
 
-  $('#hotmode').on('click', function() {
+  $('#hotmode').on('click', async function() {
     $('#slider .ui-slider-range-min').removeClass('power home hot cold');
     $('#slider .ui-slider-range-min').toggleClass('hot');
-    terminal.setSolidColor(255,0,0);
+    if (isSendData) {
+      await terminal.setSolidColor(255,0,0);
+    }
   });
 
   
-  $('#coldmode').on('click', function() {
+  $('#coldmode').on('click', async function() {
     $('#slider .ui-slider-range-min').removeClass('power home hot cold');
     $('#slider .ui-slider-range-min').toggleClass('cold');
-    terminal.setSolidColor(0,255,255);
+    if (isSendData) {
+      await terminal.setSolidColor(0,255,255);
+    }
   });
 
-  $('#homemode').on('click', function() {
+  $('#homemode').on('click', async function() {
     $('#slider .ui-slider-range-min').removeClass('power home hot cold');
     $('#slider .ui-slider-range-min').toggleClass('home');
-    terminal.setSolidColor(242,116,5);
+    if (isSendData) {
+      await terminal.setSolidColor(242,116,5);
+    }
   });
 
 
 });
 
+
+async function setPowerUI(isOn = false) {
+  console.log('isSendData: ' + isSendData);
+  if (!isOn) {
+    slider.slider( "disable" );
+    $('#hotmode, #coldmode, #homemode').attr('disabled',true);
+    if (isSendData) {
+      r = 0, g = 0, b = 0;
+      await terminal.setPowerOff();
+    }
+    
+  } else {
+
+    slider.slider( "enable" );
+    $('#hotmode, #coldmode, #homemode').attr('disabled',false);
+    if (isSendData) {
+      r = 242, g = 116, b = 5;
+      await terminal.setPowerOn();
+    }
+
+  }
+}
 
 function spinner(display = 'none') {
   if (display == 'none') {
